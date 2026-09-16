89dabe2122b229a5f3cd1ff5dd8da4ec54b0ef58
hanzhi0209 <hanzhiyin247@gmail.com>
2026-09-16 18:57:24 +0800

    Delete 启动预览.bat

--- 启动预览.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Window Archive Preview
-set "PATH=D:\大三\Kimi\resources\resources\runtime;%PATH%"
-cd /d "%~dp0"
-start "" http://localhost:7100/
-npm run dev -- --port 7100
-pause
